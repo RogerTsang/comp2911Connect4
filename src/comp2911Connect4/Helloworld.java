9ab1992e8395c0c378fccee948298be48842b680
Roger Tsang <zenghanzhang46@gmail.com>
2015-05-07 20:54:16 +1000

    First Helloworld Java through github
    Recompile and run it

---
+++ src/comp2911Connect4/Helloworld.java
@@ -0,0 +1,10 @@
+package comp2911Connect4;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
