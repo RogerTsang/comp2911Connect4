5312ad4772ea51c7603828d95ab7ae8b17ce77fd
pgilfillan <omaly321@hotmail.com>
2015-05-19 15:43:46 +1000

    Cleaned up code
    Clean-up includes changing to inner classes for the listeners rather
    than GameWindow implementing listeners, changing translateClick to
    translateMouse which now returns an int, so now the action methods
    handle updating the UI based on what column translateMouse returns.
    Will now work on converting code to new layout.

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -4,12 +4,13 @@ import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,7 +18,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class GameWindow extends JFrame implements ActionListener,MouseListener,MouseMotionListener{
+public class GameWindow extends JFrame {
 	
 	//Game g;
 	private GameBoardPanel boardPanel;
@@ -30,6 +31,11 @@ public class GameWindow extends JFrame implements ActionListener,MouseListener,M
 	
 	//current column mouse point at
 	private int col;
+	
+	//Info panels and panel for showing piece above column
+	JPanel p1Info;
+	JPanel p2Info;
+	JPanel aboveBoardPanel;
 	
 	public GameWindow(IController g) {
 		gameController = g;
@@ -55,8 +61,9 @@ public class GameWindow extends JFrame implements ActionListener,MouseListener,M
 		//But it is not of course.
 		this.setCursor(new Cursor(3));
 		//Add Mouse Listener
-		boardPanel.addMouseListener(this);
-		boardPanel.addMouseMotionListener(this);
+		MouseAction mAction = new MouseAction();
+		boardPanel.addMouseListener(mAction);
+		boardPanel.addMouseMotionListener(mAction);
 	}
 	
 	private void initUI() {
@@ -70,25 +77,25 @@ public class GameWindow extends JFrame implements ActionListener,MouseListener,M
 		//quit button
 		JButton quitButton = new JButton("Quit");
 		quitButton.setSize(50, 25);
-		quitButton.addActionListener(this);
+		quitButton.addActionListener(new ButtonAction());
 		//restart button
 		JButton restartButton = new JButton("Restart");
-		restartButton.addActionListener(this);
+		restartButton.addActionListener(new ButtonAction());
 		//restart with AI button
 		JButton restartAIButton = new JButton("EnableAI");
-		restartAIButton.addActionListener(this);
+		restartAIButton.addActionListener(new ButtonAction());
 		//undo button
 		JButton undoButton = new JButton("Undo");
-		undoButton.addActionListener(this);
+		undoButton.addActionListener(new ButtonAction());
 		//redo button
 		JButton redoButton = new JButton("Redo");
-		redoButton.addActionListener(this);
+		redoButton.addActionListener(new ButtonAction());
 		//Getscore button
 		JButton scoreButton = new JButton("Score");
-		scoreButton.addActionListener(this);
+		scoreButton.addActionListener(new ButtonAction());
 		//Request AI move button
 		JButton AIMove = new JButton("AIMove");
-		AIMove.addActionListener(this);
+		AIMove.addActionListener(new ButtonAction());
 		
 		optionsPanel.add(quitButton);
 		optionsPanel.add(restartButton);
@@ -119,13 +126,12 @@ public class GameWindow extends JFrame implements ActionListener,MouseListener,M
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	private void translateClick(int x, int boardWidth) {
+	private int translateMouse(int x, int boardWidth) {
 		int buffer = 3;
-		if (x%(boardWidth/7) < buffer || (x+buffer)%(boardWidth/7) < buffer) return;
+		if (x%(boardWidth/7) < buffer || (x+buffer)%(boardWidth/7) < buffer) return - 1;
 		else {
 			int nextMove = (int) Math.floor(x/(boardWidth/7));
-			this.gameController.move(nextMove);
-			updateUI();
+			return nextMove;
 		}
 		
 	}
@@ -149,96 +155,65 @@ public class GameWindow extends JFrame implements ActionListener,MouseListener,M
 		});
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch (e.getActionCommand()){
-		case "Quit":System.exit(0);
-					break;
-		case "Restart":gameController.newGame();
-					  gameController.detachAI();
-					  gameController.startGame();
-					  break;
-		case "EnableAI":gameController.newGame();
-		  				   gameController.attachAI(new EasyAI(Player.P2));
-		  				   gameController.startGame();
-		  				   break;
-		case "AIMove": gameController.getAITurn();
-					   break;
-		case "Undo":gameController.undo();
-					break;
-		case "Redo":gameController.redo();
-					break;
-		case "Score":gameController.getPlayerScore(Player.P1);
-					break;
-		default:break;
+	public class ButtonAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			switch (e.getActionCommand()){
+			case "Quit":System.exit(0);
+						break;
+			case "Restart":gameController.newGame();
+						  gameController.detachAI();
+						  gameController.startGame();
+						  break;
+			case "EnableAI":gameController.newGame();
+			  				   gameController.attachAI(new EasyAI(Player.P2));
+			  				   gameController.startGame();
+			  				   break;
+			case "AIMove": gameController.getAITurn();
+						   break;
+			case "Undo":gameController.undo();
+						break;
+			case "Redo":gameController.redo();
+						break;
+			case "Score":gameController.getPlayerScore(Player.P1);
+						break;
+			default:break;
+			}
+			//We need to update GUI when action is done
+			//So I put the following two lines here
+			boardPanel.update(gameController.getBoard());
+			boardPanel.updateUI();
+			
+			label.removeAll();
+			label.setText(gameController.getInfo());
+			label.updateUI();
 		}
-		//We need to update GUI when action is done
-		//So I put the following two lines here
-		boardPanel.update(gameController.getBoard());
-		boardPanel.updateUI();
-		
-		this.label.removeAll();
-		this.label.setText(gameController.getInfo());
-		label.updateUI();
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1) translateClick(e.getX(), boardPanel.getWidth());
-		this.label.removeAll();
-		this.label.setText(gameController.getInfo());
-		label.updateUI();
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
 		
 	}
 	
-	@Override
-	public void mouseMoved(MouseEvent e){
-		int boardWidth = boardPanel.getWidth();
-		int x = e.getX();
-		int buffer = 3;
-		if (x%(boardWidth/7) < buffer || (x+buffer)%(boardWidth/7) < buffer) return;
-		else {
-			int col = (int) Math.floor(x/(boardWidth/7));
-			if(col == this.col) return;
+	public class MouseAction extends MouseAdapter {
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			int nextMove = -1;
+			if (e.getButton() == MouseEvent.BUTTON1) nextMove = translateMouse(e.getX(), boardPanel.getWidth());
+			if (nextMove >= 0 && nextMove <= 6) gameController.move(nextMove);
+			updateUI();
+			label.removeAll();
+			label.setText(gameController.getInfo());
+			label.updateUI();
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e){
+			int column = translateMouse(e.getX(), boardPanel.getWidth());
+			if(column == col) return;
 			else{
+				col = column;
 				boardPanel.highlightCol(gameController.getBoard(), gameController.getCurrentPlayer(), col);
 				boardPanel.updateUI();
-				this.col = col;
 			}
 		}
 	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-		
 		
 }
 
