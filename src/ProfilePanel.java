790e1dfe4cd470c29c4bf196e116c167cb9e2ead
pgilfillan <omaly321@hotmail.com>
2015-05-28 19:58:24 +1000

    Made players name bigger

--- src/ProfilePanel.java
+++ src/ProfilePanel.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,11 +18,9 @@ public class ProfilePanel extends JPanel {
 	private ImageIcon avatar;
 	private JLabel name;
 	private JLabel gamesPlayed;
-	private JLabel ratioLabel;
 	private JLabel wlRatioH;
 	private JLabel wlRatioAIExperienced;
 	private JLabel wlRatioAINovice;
-	private JLabel spacer;
 	private JToggleButton stats;
 	
 	public ProfilePanel(Profile p) {
@@ -60,53 +59,31 @@ public class ProfilePanel extends JPanel {
 		
 		//Name of profile
 		name = new JLabel();
+		//name.setBorder(BorderFactory.createEmptyBorder(top, left, bottom, right));
+		name.setFont(name.getFont().deriveFont(Font.BOLD, 24));
 		name.setAlignmentX(Component.CENTER_ALIGNMENT);
 		//Roger: This line is used for general purpose for setting
 		if (playerOne) {
-			avatar = new ImageIcon("res/Circle_Red.png");
+			avatar = new ImageIcon(getClass().getResource("/Circle_Red.png"));
 			playerOne = false;
 		} else {
-			avatar = new ImageIcon("res/Circle_Green.png");
+			avatar = new ImageIcon(getClass().getResource("/Circle_Green.png"));
 			playerOne = true;
 		}
 		name.setIcon(avatar);
 		
 		//All the stats related to 
 		gamesPlayed = new JLabel();
-		//gamesPlayed.setText("No. Games Played: " + p.getNumGamesPlayed());
-		//gamesPlayed.setAlignmentX(Component.CENTER_ALIGNMENT);
-		ratioLabel = new JLabel();
-		//ratioLabel.setText("Ratios:");
-		//ratioLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		spacer = new JLabel();
-		//spacer.setText(" ");
-		//spacer.setAlignmentX(Component.CENTER_ALIGNMENT);
+		gamesPlayed.setAlignmentX(Component.CENTER_ALIGNMENT);
 		wlRatioH = new JLabel();
-		//wlRatioH.setText("Against Humans: " + String.format("%.2f",p.getWLRatioH()));
-		//wlRatioH.setAlignmentX(Component.CENTER_ALIGNMENT);
+		wlRatioH.setAlignmentX(Component.CENTER_ALIGNMENT);
 		wlRatioAIExperienced =  new JLabel();
-		//wlRatioAIExperienced.setText("Against Experienced AI: " + String.format("%.2f",p.getWLRatioAIExperienced()));
-		//wlRatioAIExperienced.setAlignmentX(Component.CENTER_ALIGNMENT);
+		wlRatioAIExperienced.setAlignmentX(Component.CENTER_ALIGNMENT);
 		wlRatioAINovice = new JLabel();
-		//wlRatioAINovice.setText("Against Novice AI: " + String.format("%.2f",p.getWLRatioAINovice()));
-		//wlRatioAINovice.setAlignmentX(Component.CENTER_ALIGNMENT);
+		wlRatioAINovice.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-		gamesPlayed.setVisible(false);
-		ratioLabel.setVisible(false);
-		wlRatioH.setVisible(false);
-		wlRatioAIExperienced.setVisible(false);
-		wlRatioAINovice.setVisible(false);
-		spacer.setVisible(false);
-		add(name);
-		add(stats);
-		add(gamesPlayed);
-		add(spacer);
-		add(ratioLabel);
-		add(wlRatioH);
-		add(wlRatioAIExperienced);
-		add(wlRatioAINovice);
 	}
-	
+
 	public void setProfile(Profile p) {
 		name.setText(p.getName());
 		gamesPlayed.setText("Number of games played: " + p.getNumGamesPlayed());
@@ -121,27 +98,16 @@ public class ProfilePanel extends JPanel {
 		this.name.setText(aiName);
 	}
 	
-	public void update(Profile p){
-		this.removeAll();
-		this.initUI();
-		this.setProfile(p);
-		this.updateUI();
-	}
-	
 	public class StatButtonListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (gamesPlayed.isVisible() == false) {
 				gamesPlayed.setVisible(true);
-				spacer.setVisible(true);
-				ratioLabel.setVisible(true);
 				wlRatioH.setVisible(true);
 				wlRatioAIExperienced.setVisible(true);
 				wlRatioAINovice.setVisible(true);
 			} else {
 				gamesPlayed.setVisible(false);
-				spacer.setVisible(false);
-				ratioLabel.setVisible(false);
 				wlRatioH.setVisible(false);
 				wlRatioAIExperienced.setVisible(false);
 				wlRatioAINovice.setVisible(false);
